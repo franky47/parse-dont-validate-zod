71ff52907af0596064f048723cdc48fbcac53345
Francois Best <github@francoisbest.com>
2023-04-12 11:00:14 +0200

    feat: Showcase composition

--- src/02-composition.ts
+++ src/02-composition.ts
@@ -8,3 +8,10 @@ export type User = z.infer<typeof userSchema>
 
 export const userListSchema = z.array(userSchema)
 export type UserList = z.infer<typeof userListSchema>
+
+// GraphQL-like pick & match
+
+export const userName = userSchema.pick({
+  name: true,
+})
+export type UserName = z.infer<typeof userName>
